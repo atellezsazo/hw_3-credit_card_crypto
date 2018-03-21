11ea5d3110e88ac99642a61bbba010df4decf08b
aidahalitaj <aida.halitaj@uni-pr.edu>
2018-03-22 01:21:20 +0800

    Fixing rubocop issues

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,7 @@
+
+# Implementation of Substitution Cipher
 module SubstitutionCipher
+  # Caesar Cipher
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -21,6 +24,7 @@ module SubstitutionCipher
     end
   end
 
+  # Permutation Cipher
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -32,7 +36,7 @@ module SubstitutionCipher
       document = document.to_s.chars
       rnd = (0..127).to_a.shuffle(random: Random.new(key))
       document.collect { |e| rnd.at(e.ord).chr }.join
-  end
+    end
 
     # Decrypts String document using integer key
     # Arguments:
@@ -44,6 +48,6 @@ module SubstitutionCipher
       document = document.to_s.chars
       rnd = (0..127).to_a.shuffle(random: Random.new(key))
       document.collect { |e| rnd.index(e.ord).chr }.join
-  end
+    end
   end
 end
