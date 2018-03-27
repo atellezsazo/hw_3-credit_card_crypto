9239c1769aec0042e1f74cb3883872944e64c189
Abner Tellez <atellez@grupopremiere.com>
2018-03-28 00:34:19 +0800

    Add and implemente gem rbnacl (#6)
    Add and implemente gem rbnacl

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -14,16 +15,26 @@ describe 'Test card info encryption' do
 
   ['SubstitutionCipher::Caesar',
    'SubstitutionCipher::Permutation',
-   'DoubleTranspositionCipher'].each do |method|
+   'DoubleTranspositionCipher',
+   'ModernSymmetricCipher'].each do |method|
     describe "Using #{method} " do
       it 'should encrypt/decrypt card information' do
         clazz = method.split('::').inject(Object) { |o, c| o.const_get c }
-        enc = clazz.encrypt(@cc, @key)
+        key = method == 'ModernSymmetricCipher' ? clazz.generate_new_key : @key
+        enc = clazz.encrypt(@cc, key)
         enc.wont_equal @cc.to_s
         enc.wont_be_nil
-        dec = clazz.decrypt(enc, @key)
+        dec = clazz.decrypt(enc, key)
         dec.must_equal @cc.to_s
       end
     end
+  end
+end
+
+describe 'generate ModernSymmetricCipher key' do
+  it 'should return a valid key' do
+    key = ModernSymmetricCipher.generate_new_key
+    key.wont_be_nil
+    key.length.must_equal(32)
   end
 end
